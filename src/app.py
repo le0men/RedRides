cebf167365f15d07a4f851bc0598371f084b62e8
Leo Lu <ll865@cornell.edu>
2025-05-01 18:43:46 -0400

    Finished some of the routes

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
 from db import db
 from flask import Flask, request
 import json
-from db import Listing, User
+from db import Ride, Request, User
 
 app = Flask(__name__)
 db_filename = "rr.db"
@@ -14,6 +14,7 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
 
+
 # generalized response formats
 def success_response(data, code=200):
     return json.dumps(data), code
@@ -22,7 +23,11 @@ def success_response(data, code=200):
 def failure_response(message, code=404):
     return json.dumps({"error": message}), code
 
+
 # Routes
+
+# ---- USERS ----
+
 
 # Get all users
 @app.route("/api/users/")
@@ -30,67 +35,154 @@ def get_users():
     users = [u.serialize() for u in User.querty.all()]
     return success_response(users)
 
+
 # Creates user
 @app.route("/api/users/", methods=["POST"])
 def create_user():
     body = json.loads(request.data)
     try:
         new_user = User(
-            name = body.get('name'),
-            email = body.get('email'),
-            phone = body.get('phone'),
-            grad_year = body.get('grad_year')
-            )
+            password=body.get("password"),
+            name=body.get("name"),
+            email=body.get("email"),
+            phone=body.get("phone"),
+            grad_year=body.get("grad_year"),
+        )
     except:
-        failure_response("part of input is not complete or invalid")
+        failure_response("Part of input is not complete or invalid", 400)
 
     if User.query.filter_by(email=body["email"]).first() is not None:
-        return failure_response("a user with that email already exists")
-    
+        return failure_response("A user with that email already exists", 400)
+
     if User.query.filter_by(phone=body["phone"]).first() is not None:
-        return failure_response("a user with that phone number already exists")
-    
+        return failure_response("A user with that phone number already exists", 400)
+
     db.session.add(new_user)
     db.session.commit()
     return success_response(new_user.serialize(), 201)
+
 
 # Get User by ID
 @app.route("/api/users/<int:user_id>/")
 def get_user(user_id):
     user = User.query.filter_by(id=user_id).first()
     if not user:
-        return failure_response("user not found")
+        return failure_response("User not found")
     return success_response(user.serialize())
+
 
 # Deletes a user from id
 @app.route("/api/users/<int:user_id>/", methods=["DELETE"])
-def delete_user(course_id):
+def delete_user(user_id):
+    user = User.query.filter_by(id=user_id).first()
+    if not user:
+        return failure_response("User not found")
+    db.session.delete(user)
+    db.session.commit()
+    return success_response(user.serialize())
+
+
+# login user
+@app.route("/api/users/login/", methods=["POST"])
+def login_user():
+    body = json.loads(request.data)
+    email = body.get("email")
+    password = body.get("password")
+
+    if (
+        email is None
+        or not isinstance(email, str)
+        or password is None
+        or not isinstance(password, str)
+    ):
+        failure_response("Part of input is not complete or invalid", 400)
+
+    if User.query.filter_by(email=body["email"]).first() is None:
+        return failure_response("Email not Found")
+
+    user = User.query.filter_by(email=email).first()
+
+    if user.password != password:
+        return failure_response("Incorrect Password", 400)
+
+    return success_response(user.serialize())
+
+
+# ---- RIDES ----
+
+
+# Get all rides
+@app.route("/api/rides/")
+def get_rides():
+    rides = [u.serialize() for u in Ride.querty.all()]
+    return success_response(rides)
+
+
+# Creates ride
+@app.route("/api/rides/", methods=["POST"])
+def create_ride():
+    body = json.loads(request.data)
+    try:
+        new_ride = Ride(
+            departure_city=body.get("departure_city"),
+            arrival_city=body.get("arrival_city"),
+            departure_time=body.get("departure_time"),
+            arrival_time=body.get("arrival_time"),
+            date=body.get("date"),
+            available_seats=body.get("available_seats"),
+            price=body.get("price"),
+            driver_id=body.get("driver_id"),
+        )
+    except:
+        failure_response("Part of input is not complete or invalid", 400)
+
+    driver = get_user(body.get("driver_id"))
+    # TODO: Check for conflicts, What is the format of the time passed in?
+    # TODO: Need to add ride to driver's rides list.
+    db.session.add(new_ride)
+    db.session.commit()
+    return success_response(new_ride.serialize(), 201)
+
+
+# Get ride by ID
+@app.route("/api/rides/<int:ride_id>/")
+def get_ride(ride_id):
+    ride = Ride.query.filter_by(id=ride_id).first()
+    if not ride:
+        return failure_response("Ride not found")
+    return success_response(ride.serialize())
+
+
+# Deletes a ride from id
+@app.route("/api/rides/<int:ride_id>/", methods=["DELETE"])
+def delete_ride(ride_id):
+    ride = Ride.query.filter_by(id=ride_id).first()
+    if not ride:
+        return failure_response("Ride not found")
+    db.session.delete(ride)
+    db.session.commit()
+    return success_response(ride.serialize())
+
+
+# add user (passenger) to listing
+@app.route("/api/rides/<int:listing_id>/<int:ride_id>/", methods=["POST"])
+def add_user_to_ride(listing_id, ride_id):
     pass
 
-# Get all listings
-@app.route("/api/listings/")
-def get_listings():
+
+# remove user (passenger) from listing
+@app.route("/api/rides/<int:listing_id>/<int:ride_id>/", methods=["DELETE"])
+def delete_user_from_ride(listing_id, ride_id):
     pass
 
-# Creates listing
-@app.route("/api/listings/", methods=["POST"])
-def create_listing():
+
+# check if user is part of listing (either as driver or as passenger)
+@app.route("/api/rides/<int:listing_id>/<int:ride_id>/", methods=["GET"])
+def check_user_in_ride(listing_id, ride_id):
     pass
 
-# Get listing by ID
-@app.route("/api/users/<int:listing_id>/")
-def get_listing(listing_id):
-    pass
 
-# Deletes a user from id
-@app.route("/api/users/<int:listing_id>/", methods=["DELETE"])
-def delete_listing(listing_id):
-    pass
-
-# add user to listing
-@app.route("/api/listings/<int:listing_id>/add/", methods=["POST"])
-def add_user(listing_id):
-    pass
+# ---- REQUESTS ----
 
 
 if __name__ == "__main__":
--- src/db.py
+++ src/db.py
@@ -17,6 +17,7 @@ association_table = db.Table(
 class User(db.Model):
     __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    password = db.Column(db.String, nullable=False)
     name = db.Column(db.String, nullable=False)
     email = db.Column(db.String, nullable=False)
     phone = db.Column(db.String, nullable=False)
@@ -52,12 +53,12 @@ class Ride(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     departure_city = db.Column(db.String, nullable=False)
     arrival_city = db.Column(db.String, nullable=False)
-    departure_time = db.Column(db.DateTime, nullable=False)
-    arrival_time = db.Column(db.DateTime, nullable=False)
-    date = db.Column(db.DateTime, nullable=False)
+    departure_time = db.Column(db.String, nullable=False)
+    arrival_time = db.Column(db.String, nullable=False)
+    date = db.Column(db.String, nullable=False)
     available_seats = db.Column(db.Integer, nullable=False)
     price = db.Column(db.Integer, nullable=False)
-    driverId = db.Column(db.Integer, nullable=False)
+    driver_id = db.Column(db.Integer, nullable=False)
     requests = db.relationship("Request", cascade="delete")
     passengers = db.relationship(
         "User", secondary=association_table, back_populates="rides"
@@ -70,10 +71,10 @@ class Ride(db.Model):
             "arrival_city": self.arrival_city,
             "departure_time": self.departure_time,
             "arrival_time": self.arrival_time,
-            "data": self.date,
+            "date": self.date,
             "available_seats": self.available_seats,
             "price": self.price,
-            "driverId": self.driverId,
+            "driver_id": self.driver_id,
             "requests": [s.serialize() for s in self.requests],
             "passengers": [s.simple_serialize() for s in self.passengers],
         }
@@ -88,7 +89,7 @@ class Ride(db.Model):
             "data": self.date,
             "available_seats": self.available_seats,
             "price": self.price,
-            "driverId": self.driverId,
+            "driverId": self.driver_id,
         }
 
 
@@ -96,14 +97,10 @@ class Request(db.Model):
     __tablename__ = "request"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
 
-    ride_id = db.Column(
-        db.Integer, db.ForeignKey("ride.id"), primary_key=True, autoincrement=True
-    )
+    ride_id = db.Column(db.Integer, db.ForeignKey("ride.id"), nullable=False)
 
     # TODO: make this reference an the id of an existing passenger
-    passenger_id = db.Column(
-        db.Integer, db.ForeignKey("user.id"), primary_key=True, autoincrement=True
-    )
+    passenger_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
 
     # must be either "yes", "no", or "pending"
     status = db.Column(db.String, nullable=False)
